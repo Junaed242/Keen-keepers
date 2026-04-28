15d372b14452fb5275e0c7fd4efdc39b96248bc7
Junaed242 <abdullah56429@gmail.com>
2026-04-28 22:50:30 +0600

    Pages setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,26 @@
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css';
+import Home from "./pages/Home";
+import FriendDetail from "./pages/FriendDetail";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    errorElement: <NotFound />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/friend/:id", element: <FriendDetail /> },
+      { path: "/timeline", element: <Timeline /> },
+      { path: "/stats", element: <Stats /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>KeenKeeper</h1>
+  <RouterProvider router={router} />
 );
